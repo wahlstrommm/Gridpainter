d9b00d975da575cf795d015dd0191fcbfcae81af
André Narvell <92329693+AndreNarvell@users.noreply.github.com>
2022-08-27 18:39:36 +0200

    Got connection yay

--- server/controllers/socket_controller.js
+++ server/controllers/socket_controller.js
@@ -13,14 +13,44 @@ const rooms = [
     users: {},
   },
   {
-    id: "room2",
-    name: "Room 2",
+    id: "room3",
+    name: "Room 3",
     users: {},
   },
 ];
 
 const getRoomById = (id) => {
   return rooms.find((room) => room.id === id);
+};
+
+const getRoomByUserId = (id) => {
+  return rooms.find((chatroom) => chatroom.users.hasOwnProperty(id));
+};
+
+const handleGetRoomList = function (callback) {
+  const room_list = rooms.map((room) => {
+    return {
+      id: room.id,
+      name: room.name,
+    };
+  });
+  callback(room_list);
+};
+
+const handleDisconnect = function () {
+  debug(`Client ${this.id} disconnected :(`);
+
+  const room = getRoomByUserId(this.id);
+
+  if (!room) {
+    return;
+  }
+
+  this.broadcast.to(room.id).emit("user:disconnected", room.users[this.id]);
+
+  delete room.users[this.id];
+
+  this.broadcast.to(room.id).emit("user:list", room.users);
 };
 
 const handleUserJoined = async function (username, room_id, callback) {
@@ -47,6 +77,12 @@ const handleUserJoined = async function (username, room_id, callback) {
 
 module.exports = function (socket, _io) {
   io = _io;
+
+  socket.on("get-room-list", handleGetRoomList);
+
+  socket.on("user:joined", handleUserJoined);
+
+  socket.on("disconnect", handleDisconnect);
 
   debug(`Client ${socket.id} connected`);
 };
