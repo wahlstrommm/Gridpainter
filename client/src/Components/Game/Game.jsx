6bd1e789f60c1bd509fda4a2ab9288f8ef1f0d4c
André Narvell <92329693+AndreNarvell@users.noreply.github.com>
2022-08-27 16:15:21 +0200

    Starting new socket server

--- client/src/Components/Game/Game.jsx
+++ client/src/Components/Game/Game.jsx
@@ -2,31 +2,34 @@ import Chat from "./Chat/Chat";
 import "./game.css";
 
 export function Game(props) {
+  const usersInfo = {
+    userName: props.props.userName,
+    roomName: props.props.roomName,
+  };
 
-    const usersInfo = { userName: props.props.userName, roomName: props.props.roomName };
+  return (
+    <>
+      <div className="pageWrapper">
+        <div className="leftSideWrapper">
+          <section className="gameInfo">
+            <h1>Game</h1>
+            <h2>Roomname: {props.props.roomName}</h2>
+            <p>Username: {props.props.userName}</p>
+          </section>
 
-    return (<>
-        <div className="pageWrapper">
-            <div className="leftSideWrapper">
-                <section className="gameInfo">
-                    <h1>Game</h1>
-                    <p>{props.props.userName}</p>
-                    <p>{props.props.roomName}</p>
-                </section>
-
-                <section className="chatSection">
-                    <Chat userInfo={usersInfo} />
-                </section>
-            </div>
-
-            <div className="gameboardWrapper">
-                <section className="gameboardSection">Gameboard</section>
-            </div>
-
-            <div className="resultboardWrapper">
-                <section className="resultboardSection">Resultboard</section>
-            </div>
-
+          <section className="chatSection">
+            <Chat userInfo={usersInfo} />
+          </section>
         </div>
-    </>);
+
+        <div className="gameboardWrapper">
+          <section className="gameboardSection">Gameboard</section>
+        </div>
+
+        <div className="resultboardWrapper">
+          <section className="resultboardSection">Resultboard</section>
+        </div>
+      </div>
+    </>
+  );
 }
