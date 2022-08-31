3d49f211713765cf79a5b1b73626ad98d744b003
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-08-31 22:37:34 +0200

    git imgs to frontend

--- client/src/components/gallery/Gallery.jsx
+++ client/src/components/gallery/Gallery.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./gallery.scss";
+import axios from "axios";
 
 const Gallery = () => {
+
+  axios.get("http://localhost:4000/img")
+  .then(res => {
+    console.log("Hej från axios, get img", res.data);
+
+    for (let i = 0; i < res.data.length; i++) {
+      console.log(res.data[i]._id);
+      console.log(res.data[i].img);
+    }
+
+    console.log(res.data);
+  })
+
   return <section>
     <h1>Galleri</h1>
+
     <div>
       <img src="" alt="Text" />
       <img src="" alt="Text" />
@@ -13,7 +28,9 @@ const Gallery = () => {
       <img src="" alt="Text" />
       <img src="" alt="Text" />
     </div>
+
     <button className="startBtn"><Link to='/'>Tillbaka</Link></button>
+
   </section>;
 };
 
